e9d4b8b5332411469865d00e4d3f2c72db67792a
Epram_Fouad <epramfouad14@gmail.com>
2024-09-08 20:41:27 +0300

    adding resources manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,49 @@
+
+import 'package:app_with_clean_arch/presentation/forget_password/forget_password_view.dart';
+import 'package:app_with_clean_arch/presentation/login/login_view.dart';
+import 'package:app_with_clean_arch/presentation/main/main_view.dart';
+import 'package:app_with_clean_arch/presentation/register/register_view.dart';
+import 'package:app_with_clean_arch/presentation/resources/strings_manager.dart';
+import 'package:app_with_clean_arch/presentation/splash/splash_view.dart';
+import 'package:app_with_clean_arch/presentation/store_details/store_details_view.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String splashRoute = "/";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgotPasswordRoute = "/forgotPassword";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+          appBar: AppBar(
+            title: const Text(AppStrings.noRouteFound),
+          ),
+          body: const Center(child: Text(AppStrings.noRouteFound)),
+        ));
+  }
+}
